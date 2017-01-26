a90d1ef586e3c56dc2a06f5b74a17855f999603a
Bryan Maass <bryan.maass@gmail.com>
2017-01-26 21:58:41 +1100

    oh yeah weighting stuff

--- src/cljs/weighter/core.cljs
+++ src/cljs/weighter/core.cljs
@@ -12,12 +12,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; DB
 
-(def schema {:task/category         {:db/valueType :db.type/ref}
-             :category/todo         {:db/valueType :db.type/ref}
-             :todo/display-category {:db/valueType :db.type/ref}
-             :task/name             {:db/unique :db.unique/identity}
-             :todo/name             {:db/unique :db.unique/identity}
-             :action/editing        {:db/cardinality :db.cardinality/many}})
+(def schema {})
 
 (defn new-entity! [conn varmap]
   ((:tempids (d/transact! conn [(merge varmap {:db/id -1})])) -1))
@@ -25,107 +20,112 @@
 (defonce tempid (let [n (atom 0)] (fn [] (swap! n dec))))
 
 (defn populate! [conn]
-  (let [todo-id    (new-entity! conn {:todo/name "Matt's List" :todo/listing :all})
-        at-home    (new-entity! conn {:category/name "At Home" :category/todo todo-id})
-        work-stuff (new-entity! conn {:category/name "Work Stuff" :category/todo todo-id})
-        hobby      (new-entity! conn {:category/name "Hobby" :category/todo todo-id})]
-    (d/transact!
-     conn
-     [{:db/id (tempid) :task/name "Clean Dishes" :task/done true :task/category at-home}
-      {:db/id (tempid) :task/name "Mop Floors" :task/done true :task/pinned true :task/category at-home}
-      {:db/id (tempid) :task/name "Draw a picture of a cat" :task/done false :task/category hobby}
-      {:db/id (tempid) :task/name "Compose opera" :task/done true :task/category hobby}
-      {:db/id (tempid) :task/name "stock market library"
-       :task/done false :task/pinned true :task/category work-stuff}])))
+  (d/transact!
+   conn
+   [{:db/id (tempid)
+     :item/title "House Size"
+     :item/value 5
+     :item/weight 5}
+    {:db/id (tempid)
+     :item/title "House Location"
+     :item/value 5
+     :item/weight 10}]))
 
-(defonce app-state (let [db (d/create-conn schema)]
-                     (populate! db)
-                     (p/posh! db)
-                     db))
+(defonce conn (let [conn (d/create-conn schema)]
+                (populate! conn)
+                (p/posh! conn)
+                conn))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; subs
 
-(defn todos [conn]
-  (p/q '[:find ?tid ?name ?done ?category
+(defn item-ids [conn]
+  (p/q '[:find ?eid
+         :in $
+         :where [?eid :item/title _]] conn))
+
+(defn pull-item [conn id]
+  (p/pull conn '[:item/title
+                 :item/value
+                 :item/weight] id))
+
+(defn total [conn]
+  (p/q '[:find ?weight ?value ?eid
          :in $
          :where
-         [?tid :task/name ?name]
-         [?tid :task/done ?done]
-         [?tid :task/category ?cid]
-         [?cid :category/name ?category]] conn))
-
-(defn done-todos [conn]
-  (p/q '[:find ?tid ?name ?done ?category
-         :in $
-         :where [?tid :task/name ?name]
-         [?tid :task/done true]
-         [?tid :task/done ?done]
-         [?tid :task/category ?cid]
-         [?cid :category/name ?category]] conn))
+         [?eid :item/weight ?weight]
+         [?eid :item/value ?value]] conn))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; subs
+;; Page
 
-(defn listing [conn]
-  (p/q '[:find ?listing
-         :in $
-         :where
-         [_ :todo/listing ?listing]]
-       conn))
+(defn total-view [conn]
+  (let [total (total conn)]
+    (fn [conn]
+      [:h1 (apply + (for [[weight value _] @total]
+                       (* (/ weight 100) value)))])))
+
+(defn display-item [conn id]
+  (let [item (pull-item conn id)]
+    (fn [conn]
+      (let [{:keys [item/title item/value item/weight db/id]} @item]
+        [:div {:style {:display "flex"
+                       :flex-flow "column wrap"
+                       :border "2px solid black"
+                       :border-radius "3px"
+                       :padding "10px"
+                       :margin "10px"}}
+         [:div {:style {:font-size "20px"}} title]
+         [:div {:style {:flex-flow "row nowrap"}}
+          [:div
+           [:div [:span {:style {:display "inline-block" :width "100px"}} "Value "]
+            [:input {:type :range :min 0 :max 10 :step 0.5 :value value
+                     :on-change (fn [e]
+                                  (p/transact!
+                                   conn [[:db/add id
+                                          :item/value (-> e .-target .-value)]]))}]
+            [:span value]]]
+          [:div
+           [:div [:span {:style {:display "inline-block" :width "100px"}} "Weight "]
+            [:input {:type :range :min 0 :max 10 :step 0.5 :value weight
+                     :on-change (fn [e]
+                                  (p/transact!
+                                   conn [[:db/add id
+                                          :item/weight (-> e .-target .-value)]]))}]
+            [:span weight]]]]]))))
 
 (defn page [conn]
-  (let [listing (listing conn)
-        todos (todos conn)
-        done-todos (done-todos conn)
-        ]
+  (let [item-ids (item-ids conn)]
     (fn [conn]
-      [:div
-       [:pre (pr-str (ffirst @listing))]
-       ;;[:h1 [:pre (pr-str conn)]]
-       ;;[:pre (pr-str @(p/q '[:find [?e] :in $ :where [?e _ _]] conn))]
-       [:div {:style {:border "1px black solid"
-                      :border-radius "2px"
-                      :margin "10px"
-                      :padding "10px"}}
-        [:h3 "All Todos"]
-        (into [:div]
-              (for [[id name done category] @todos]
-                [:div
-                 [:input {:type :checkbox
-                          :default-checked done
-                          :on-click #(p/transact!
-                                      conn [[:db/add id
-                                             :task/done (not done)]])}]
-                 name " (" category ")"]))]
-       [:div {:style {:border "1px black solid"
-                      :border-radius "2px"
-                      :margin "10px"
-                      :padding "10px"}}
-        [:h3 "Done Todos"]
-        (into [:div]
-              (for [[id name] @done-todos]
-                [:div name]))]])))
+      [:div {:style {:display "flexbox" :flex-flow "column nowrap"}}
+       [total-view conn]
+       (into [:div {:style {:display "flexbox" :flex-flow "row wrap"}}]
+             (for [[item-id] @item-ids]
+               [display-item conn item-id]))
+       [:input {:type "button"
+                :on-click #(d/transact!
+                            conn
+                            [{:db/id (tempid)
+                              :item/title "XXX"
+                              :item/value 5
+                              :item/weight 5}])
+                :value "+"}]])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initialize App
 
 (defn reload []
-  (reagent/render [page app-state]
+  (reagent/render [page conn]
                   (.getElementById js/document "app")))
 
 (defn dev-setup []
   (when debug?
     (enable-console-print!)
     (rf/enable-frisk!)
-    (rf/add-data :db (pr-str @app-state))
-    (rf/add-data :todos (pr-str @(todos app-state)))
+    (rf/add-data :db (pr-str @conn))
     (println "dev mode")
     (devtools/install!)))
 
 (defn ^:export main []
   (dev-setup)
   (reload))
-
-
-(rf/add-data :todos (pr-str @(todos app-state)))
